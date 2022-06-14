9a98e7f995d89049da0083d59ce13c65d5bc765a
Justin DeFrank <justin.defrank@gmail.com>
2022-06-14 13:02:20 -0400

    Update variables.tf
    Removing deprecated variable type strings

--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Subnet name to create in GCP"
 }
 
 variable "description" {
-  type        = "string"
+  type        = string
   description = "Description of subnet to create in GCP"
   default     = "a subnet"
 }
 
 variable "vpc" {
-  type        = "string"
+  type        = string
   description = "Direct link to the network"
 }
 
 variable "subnetwork-region" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Zone associated with the subnet. This defaults to the region configured in the provider."
 }
 
 variable "ip_cidr_range" {
-  type        = "string"
+  type        = string
   description = "IP range to book"
 }
